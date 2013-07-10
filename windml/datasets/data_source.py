be5382b66ea958a5cdeaf50d3b0a4308ec4b3f12
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2013-07-10 14:03:07 +0200

    added docstrings

--- windml/datasets/data_source.py
+++ windml/datasets/data_source.py
@@ -1,7 +1,45 @@
 class DataSource(object):
+    """Abstract class of data sources"""
+
     def get_windpark(self, target_idx, radius, year_from=0, year_to=0):
-        pass
-    def get_windmill(self, target_idx, year_from=0, year_to=0):
+        """This method returns a Windpark object of a given data source.
+
+        Parameters
+        ----------
+
+        target_idx : int
+                     Depends on the data source.
+        year_from  : int
+                     2004 - 2006
+        year_to    : int
+                     2004 - 2006
+
+        Returns
+        -------
+
+        Windpark
+            An according windpark for target id, radius and time span.
+        """
         pass
 
+    def get_windmill(self, target_idx, year_from=0, year_to=0):
+        """This method returns a Windmill object of a given data source.
+
+        Parameters
+        ----------
+
+        target_idx : int
+                     Depends on the data source.
+        year_from  : int
+                     2004 - 2006
+        year_to    : int
+                     2004 - 2006
+
+        Returns
+        -------
+
+        Windmill
+            An according windmill for target id and time span.
+        """
+        pass
 
