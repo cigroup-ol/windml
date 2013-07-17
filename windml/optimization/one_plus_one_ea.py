ff6d02982bcfb1630b15e73b68161f050a2a9054
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2013-07-17 14:32:11 +0200

    added license badge to main source files

--- windml/optimization/one_plus_one_ea.py
+++ windml/optimization/one_plus_one_ea.py
@@ -1,3 +1,36 @@
+"""
+Copyright (c) 2013,
+Fabian Gieseke, Justin P. Heinermann, Oliver Kramer, Jendrik Poloczek,
+Nils A. Treiber
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    Neither the name of the Computational Intelligence Group of the University
+    of Oldenburg nor the names of its contributors may be used to endorse or
+    promote products derived from this software without specific prior written
+    permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from numpy.random import normal, rand
 
 class OnePlusOneEA(object):
@@ -7,7 +40,7 @@ class OnePlusOneEA(object):
         """
         A (1+1)-EA for real parameter optimization of an algorithm. Does
         not adapt step size, the choice of sigma is therefore very important.
-    
+
         parameter: the name of the real parameter to minimize
         mean: mean of population and start position respectively
         sigma: step size of the evolution process
@@ -16,19 +49,19 @@ class OnePlusOneEA(object):
         args: hashmap of the arguments of algorithm
         algorithm: the algorithm function to optimize, the function
         needs to accept the args hashmap.
-    
+
         Example: Optimizing a custom algorithm
         --------------------------------------------
-    
+
         def algorithm(args):
             # here return the error to minimize
             return args['radius'] ** 2
-    
+
         args = {'otherarg' : 1}
         inte = [0, 10]
         optradius, error = minimize11ea('radius', 1.0, 5.0, inte, 50, args, algorithm)
         """
-    
+
         feasible = False
         while(not feasible):
             parent = normal(mean, sigma)
@@ -38,7 +71,7 @@ class OnePlusOneEA(object):
 
         args[parameter] = parent
         best_error = algorithm(args)
-    
+
         for i in xrange(iterations):
             feasible = False
             while(not feasible):
@@ -52,6 +85,6 @@ class OnePlusOneEA(object):
                 parent = offspring
                 best_error = error
                 print parent, error
-    
+
         return parent, best_error
 
--- windml/util/features.py
+++ windml/util/features.py
@@ -1,9 +1,35 @@
 """
-    Computation of features for a wind turbine
-    --------------------------------------------------
-    
-    ...
-    """
+Copyright (c) 2013,
+Fabian Gieseke, Justin P. Heinermann, Oliver Kramer, Jendrik Poloczek,
+Nils A. Treiber
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    Neither the name of the Computational Intelligence Group of the University
+    of Oldenburg nor the names of its contributors may be used to endorse or
+    promote products derived from this software without specific prior written
+    permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 import numpy as np
 
@@ -15,12 +41,12 @@ def compute_highlevel_features(turbine, power_features = True, ramp_features = T
     X = np.array([m[1] for m in turbine.get_measurements()])
     feat = []
     month_power = []
-    
-    
+
+
     """
-    power features    
+    power features
     """
-    
+
 # sum of power a year
 
     x =  np.sum(X)
@@ -57,7 +83,7 @@ def compute_highlevel_features(turbine, power_features = True, ramp_features = T
     """
     ramp features
     """
-    
+
 # measured features
     ramps_up = []
     ramps_down = []
