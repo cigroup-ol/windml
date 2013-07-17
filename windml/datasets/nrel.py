29d4792fa4308fbb671a9755bb86e8be97911910
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2013-07-17 14:23:38 +0200

    added NREL license badge

--- windml/datasets/nrel.py
+++ windml/datasets/nrel.py
@@ -26,6 +26,38 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----
+
+This data and software ("Data") is provided by the National Renewable Energy
+Laboratory ("NREL"), which is operated by the Alliance for Sustainable Energy,
+LLC ("ALLIANCE") for the U.S. Department Of Energy ("DOE").
+
+Access to and use of these Data shall impose the following obligations on the
+user, as set forth in this Agreement.  The user is granted the right, without
+any fee or cost, to use, copy, modify, alter, enhance and distribute these Data
+for any purpose whatsoever, provided that this entire notice appears in all
+copies of the Data.  Further, the user agrees to credit DOE/NREL/ALLIANCE in
+any publication that results from the use of these Data.  The names
+DOE/NREL/ALLIANCE, however, may not be used in any advertising or publicity to
+endorse or promote any products or commercial entities unless specific written
+permission is obtained from DOE/NREL/ ALLIANCE.  The user also understands that
+DOE/NREL/Alliance is not obligated to provide the user with any support,
+consulting, training or assistance of any kind with regard to the use of these
+Data or to provide the user with any updates, revisions or new versions of
+these Data.
+
+YOU AGREE TO INDEMNIFY DOE/NREL/Alliance, AND ITS SUBSIDIARIES, AFFILIATES,
+OFFICERS, AGENTS, AND EMPLOYEES AGAINST ANY CLAIM OR DEMAND, INCLUDING
+REASONABLE ATTORNEYS' FEES, RELATED TO YOUR USE OF THESE DATA.  THESE DATA ARE
+PROVIDED BY DOE/NREL/Alliance "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+DOE/NREL/ALLIANCE BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES
+OR ANY DAMAGES WHATSOEVER, INCLUDING BUT NOT LIMITED TO CLAIMS ASSOCIATED WITH
+THE LOSS OF DATA OR PROFITS, WHICH MAY RESULT FROM AN ACTION IN CONTRACT,
+NEGLIGENCE OR OTHER TORTIOUS CLAIM THAT ARISES OUT OF OR IN CONNECTION WITH THE
+ACCESS, USE OR PERFORMANCE OF THESE DATA.
 """
 
 import os
