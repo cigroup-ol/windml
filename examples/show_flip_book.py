e983cc58aa2afb867ced26fb8ab33a635b1c6500
Nils <stereo@vegas.informatik.uni-oldenburg.de>
2013-07-17 11:55:55 +0200

    Docu of Example: Flip-Book

--- examples/show_flip_book.py
+++ examples/show_flip_book.py
@@ -1,6 +1,11 @@
 """
 Flip-Book of Wind Speed Around Tehachapi
 -------------------------------------------------------------------------
+
+In this example illustrates the wind speed of mills in Tehachapi. The figure visualizes 
+the wind situation at four different times (with a difference of 20 min). The turbines are colorized
+with regard to the wind strengths (from strong in red to low in blue).
+
 """
 
 from windml.datasets.nrel import NREL
