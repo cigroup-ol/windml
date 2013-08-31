8cf1fd4c124f15866f18509a65372c3c9eaca909
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2013-08-31 16:33:54 +0200

    modified version number in __init__.py

--- windml/__init__.py
+++ windml/__init__.py
@@ -1,13 +1,2 @@
-"""A pypi demonstration vehicle.
-
-.. moduleauthor:: Andrew Carter <andrew@invalid.com>
-
-"""
-
-
-def start():
-    "This starts this module running ..."
-
 __all__ = []
-__version__ = '0.1'
-
+__version__ = '0.3'
