8ed62f6f6662bb9a70c78b5e1a819a4e8fe4af5f
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2013-11-12 11:02:36 +0100

    added example description

--- examples/benchmark/comparison.py
+++ examples/benchmark/comparison.py
@@ -1,3 +1,17 @@
+"""
+MSE Comparison of Predictors
+-------------------------------------------------------------------------
+
+This example shows a comparison of different predictors: Standard
+spatio-temporal predictor and standard spatio-temporal predictor with
+a smooth training set. The testbed is QuickNDirty, which is only used
+for the example page presentation.
+"""
+
+# Author: Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
+# Author: Nils A. Treiber <nils.andre.treiber@uni-oldenburg.de>
+# License: BSD 3 clause
+
 from windml.benchmark.benchmark import Benchmark
 from windml.prediction.std_linreg import StdLinreg
 from windml.prediction.smoothed_linreg import SmoothedLinreg
