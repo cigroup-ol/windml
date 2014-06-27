be612a7af6c0cebc3fe89b54e16b7676fed8d5cf
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2014-06-27 12:26:06 +0200

    updated init file with project details

--- windml/__init__.py
+++ windml/__init__.py
@@ -1,2 +1,13 @@
-__all__ = []
-__version__ = '0.3'
+# -*- coding: utf-8 -*-
+"""
+Root package of MetaOpt.
+"""
+__author__ = 'Nils André Treiber, Jendrik Poloczek, Oliver Kramer, Justin Philipp Heinermann, Fabian Gieseke'
+__author_email__ = 'nils.andre.treiber@uni-oldenburg.de'
+__license__ = 'BSD'
+# __maintainer__ = "first last"
+# __maintainer_email__ = "first.last@example.com"
+__url__ = 'http://windml.org/'
+__version__ = '0.5.0'
+
+
